761bb9722820ed8e6bee842351c62dc516c27243
ho-ya-kim <seoho7777.kim@gmail.com>
2024-11-02 03:12:57 +0900

    separate mobile/desktop

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,8 +1,6 @@
 import { mysqlTable, serial, text, } from "drizzle-orm/mysql-core";
 // 각 type은 공식문서 참고
 
-// EXAMPLE:
-//
 export const users = mysqlTable('users', {
     id: serial('id').primaryKey(),
     name: text('name').notNull(),
