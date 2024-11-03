0c2e288b037ca7b24c33f13c8522e61894c92889
ho-ya-kim <seoho7777.kim@gmail.com>
2024-11-03 23:11:51 +0900

    20241103
    add signup (both mobile & desktop)

--- src/app/(pages)/mobile.tsx
+++ src/app/(pages)/mobile.tsx
@@ -1,47 +1,99 @@
+"use client";
+
 import Link from "next/link";
+import React, {useState} from "react";
+import {cn} from "@/app/components/functions";
 
 export default function Mobile() {
+    const [showPassword, setShowPassword] = useState(false);
+    const [isLogin, setIsLogin] = useState(true);
     return (
         <>
             {/* 부모 컨테이너에 Flexbox 적용 */}
-            <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-900 p-4">
+            <div className="component-container p-4">
+                <div className="grid grid-cols-2 component-form mb-4 p-0 max-w-xs">
+                    <button
+                        className={cn({"bg-white pointer-events-none": isLogin, "bg-gray-300 hover:bg-gray-200 transition duration-200": !isLogin}, "h-fit rounded-l-lg text-center p-2")}
+                        onClick={() => setIsLogin(!isLogin)}
+                    >
+                        Login
+                    </button>
+                    <button
+                        className={cn({"bg-white pointer-events-none": !isLogin, "bg-gray-300 hover:bg-gray-200 transition duration-200": isLogin}, "h-fit rounded-r-lg text-center p-2")}
+                        onClick={() => setIsLogin(!isLogin)}
+                    >
+                        Sign Up
+                    </button>
+                </div>
                 <div className="w-full max-w-xs bg-white dark:bg-gray-800 rounded-lg p-6">
-                    <h2 className="text-xl font-semibold text-center text-gray-700 dark:text-gray-200 mb-4">
-                        로그인
+                    <h2 className="title-2">
+                        {isLogin ? "로그인" : "회원 가입"}
                     </h2>
                     <form className="space-y-4">
                         <div>
-                            <label htmlFor="name" className="block text-sm font-medium text-gray-600 dark:text-gray-300 mb-1">
+                            <label htmlFor="name" className="component-button-info">
                                 Name:
                             </label>
                             <input
                                 type="text"
                                 id="name"
-                                className="w-full px-3 py-2 border rounded-lg border-gray-300 dark:border-gray-600 focus:(outline-none ring-2 ring-blue-400) bg-gray-100 dark:bg-gray-700 text-gray-800 dark:text-gray-100"
+                                className="component-input"
                                 placeholder="Enter your name"
                                 required
                             />
                         </div>
 
                         <div>
-                            <label htmlFor="password" className="block text-sm font-medium text-gray-600 dark:text-gray-300 mb-1">
+                            <label htmlFor="password" className="component-button-info">
                                 Password:
                             </label>
-                            <input
-                                type="password"
-                                id="pw"
-                                className="w-full px-3 py-2 border rounded-lg border-gray-300 dark:border-gray-600 focus:(outline-none ring-2 ring-blue-400) bg-gray-100 dark:bg-gray-700 text-gray-800 dark:text-gray-100"
-                                placeholder="Enter your password"
-                                required
-                            />
+                            <div className="relative">
+                                <input
+                                    type={showPassword ? "text" : "password"}
+                                    id="password"
+                                    className="component-input"
+                                    placeholder="Enter your password"
+                                    required
+                                />
+                                <button
+                                    type="button"
+                                    className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 dark:text-gray-400"
+                                    onClick={() => setShowPassword(!showPassword)}
+                                >
+                                    <div className={showPassword ? "i-system-uicons-eye" : "i-system-uicons-eye-closed"}/>
+                                    {/* 비밀번호 보기 아이콘 */}
+                                </button>
+                            </div>
                         </div>
 
-                        <div className="flex justify-center">
+                        {isLogin ? <></> :
+                            <div>
+                                <label htmlFor="name" className="component-button-info">
+                                    머 적당히 가입할 때 필요한 정보들
+                                </label>
+                                <input
+                                    type="text"
+                                    id="name"
+                                    className="component-input"
+                                    placeholder="뭐가 필요할까"
+                                    required
+                                />
+                            </div>
+                        }
+
+                        <div className={cn("flex items-center content-end justify-end")}>
+                            <label className="flex items-center">
+                                <span className="text-sm text-gray-600 dark:text-gray-300">Remember me</span>
+                                <input type="checkbox" className="ml-2"/>
+                            </label>
+                        </div>
+
+                        <div className="flex flex-col items-center space-y-4">
                             <Link
-                                className="w-full text-center bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 rounded-lg transition duration-200"
+                                className="component-button"
                                 href={'/home'}
                             >
-                                Login
+                                {isLogin ? "Login" : "Sign Up"}
                             </Link>
                         </div>
                     </form>
--- src/app/components/functions.tsx
+++ src/app/components/functions.tsx
@@ -1,6 +1,8 @@
 import {clsx, ClassValue} from "clsx";
 import {twMerge} from "tailwind-merge";
 
+// easy to use conditional statements in css
+// easy to merge css
 export const cn = (...inputs: ClassValue[]) => {
     return twMerge(clsx(inputs));
 };
