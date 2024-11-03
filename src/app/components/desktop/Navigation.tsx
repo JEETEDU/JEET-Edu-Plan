fe99237ee97945254afdd5f7872c176dbb42c616
ho-ya-kim <seoho7777.kim@gmail.com>
2024-11-04 02:55:33 +0900

    20241104
    some edits...

--- src/app/components/desktop/Navigation.tsx
+++ src/app/components/desktop/Navigation.tsx
@@ -3,9 +3,14 @@
 import Link from "next/link";
 import {cn} from "@/app/components/functions";
 import {usePathname} from "next/navigation";
+// import {useCookies} from "next-client-cookies";
 
 export default function Navigation() {
     const path = usePathname();
+    // const cookies = useCookies();
+
+    // console.log(`navigation: ${cookies.get('user')}`)
+
     return (
         <div className='fixed'>
             <nav className="min-w-screen bg-gray-100 dark:bg-gray-800 flex items-center justify-between px-6 py-4">
@@ -16,7 +21,7 @@ export default function Navigation() {
                     </div>
                 </div>
                 {/* 네비게이션 링크 */}
-                <div className="flex w-1/3 justify-around">
+                <div className="flex w-1/3 min-w-fit justify-around">
                     <Link href={"/home"} className="nav-item">
                         <span className={cn("_nav-item", {"bg-white": path === '/home'})}>
                             Home
