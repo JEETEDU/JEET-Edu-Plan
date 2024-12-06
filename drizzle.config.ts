43c2e25c622d36a5df2309faf7bccc1a4f2628a9
hegelty <skxodid0305@gmail.com>
2024-12-07 05:41:03 +0900

    Implement user register api and setup orm

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,11 +3,11 @@ import {defineConfig} from 'drizzle-kit';
 export default defineConfig({
     dialect: "mysql",
     dbCredentials: {
-        host: process.env["DB_HOST"],
+        host: process.env["DB_HOST"] as string,
         port: Number(process.env["DB_PORT"]),
-        user: process.env["DB_USER"],
-        password: process.env["DB_PSWD"],
-        database: process.env["DB_NAME"],
+        user: process.env["DB_USER"] as string,
+        password: process.env["DB_PSWD"] as string,
+        database: process.env["DB_NAME"] as string,
     },
     schema: './src/database/schema.ts',
     out: './src/database/drizzle',
--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,8 +1,25 @@
-import { mysqlTable, int, text, } from "drizzle-orm/mysql-core";
+import { mysqlTable, int, char, binary, tinyint, year, varchar } from "drizzle-orm/mysql-core";
 // 각 type은 공식문서 참고
 
+/*
+CREATE TABLE `user` (
+	`uid`	INT AUTO_INCREMENT	NOT NULL,
+	`login_id`	CHAR(20)	NOT NULL,
+	`pw`	BINARY(16)	NOT NULL,
+	`user_type`	TINYINT	NOT NULL	DEFAULT 0	COMMENT '미승인: 0, 학생:1, 강사: 2, 어드민: 3',
+	`name`	CHAR(5)	NOT NULL,
+	`first_year`	YEAR	NULL	COMMENT '강사는 NULL',
+	`school`	VARCHAR(255)	NULL	COMMENT '강사는 NULL',
+	`joined_term`	VARCHAR(20)	NULL	COMMENT '강사는 NULL'
+);
+ */
 export const usersTable = mysqlTable('users', {
-    id: int('id').autoincrement().primaryKey(),
-    name: text('name').notNull(),
-    password: text('password').notNull(),
+    uid: int().autoincrement().primaryKey(),
+    login_id: char({length: 20}).notNull().unique(),
+    pw: binary({length: 32}).notNull(),
+    user_type: tinyint().notNull().default(0),
+    name: char({length: 5}).notNull(),
+    first_year: year(),
+    school: varchar({length: 255}),
+    joined_term: varchar({length: 20}),
 });
