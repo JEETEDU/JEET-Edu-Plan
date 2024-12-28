f2d06cb973a1b824545ab410d47b9654ac974567
ho-ya-kim <seoho7777.kim@gmail.com>
2024-12-28 12:49:52 +0900

    모바일 거의 끝

--- src/app/mypage/(pages)/mobile.tsx
+++ src/app/mypage/(pages)/mobile.tsx
@@ -1,7 +1,26 @@
 export default function Mobile() {
+    const info = {
+        name: "나태양",
+        grade: "G3-S",
+        school: "지트중학교",
+    };
+
     return (<>
-        <div>
-            My Page
-        </div>
-    </>)
+            <div className="w-full h-full flex flex-col items-center bg-gray-100">
+                <div className="w-full flex flex-row justify-between items-center py-3 px-6">
+                    <div className="text-4xl font-bold text-gray-800">
+                        {info.name}
+                    </div>
+                    <div className="w-fit flex flex-col items-end">
+                        <div className="text-left text-xl font-bold text-gray-800">
+                            {info.grade}
+                        </div>
+                        <div className="text-left text-l font-bold text-gray-600">
+                            {info.school}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
