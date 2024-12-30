04a89fd747267b616fc93fde560462670ad45630
hegelty <skxodid0305@gmail.com>
2024-12-31 04:30:09 +0900

    Implement some admin APIs(user accept/reject/delete)

---
+++ src/app/(others)/api/admin/user/accept/route.ts
@@ -0,0 +1,106 @@
+import type { NextRequest } from 'next/server';
+import { db } from '@/database';
+import * as schema from '@/database/schema';
+import { NextResponse } from 'next/server';
+import { eq } from 'drizzle-orm';
+import {return_400, UserType} from "@/app/(others)/api/tools";
+import {verifyToken} from "@/app/(others)/api/auth";
+
+/**
+ * @swagger
+ * /api/admin/user/accept:
+ *  post:
+ *      tags:
+ *          - Admin
+ *      description: Accept a user
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          user_id:
+ *                              type: number
+ *                              description: User's ID
+ *                              example: 1
+ *                      required:
+ *                          - user_id
+ *      responses:
+ *          "200":
+ *              description: User accepted
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              success:
+ *                                  type: boolean
+ *                                  example: true
+ *                              message:
+ *                                  type: string
+ *                                  example: "User accepted"
+ *          "400":
+ *              description: Bad request
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              success:
+ *                                  type: boolean
+ *                                  example: false
+ *                              message:
+ *                                  type: string
+ *                                  example: "error message"
+ */
+export async function POST(req: NextRequest) {
+    try {
+        const token: string = req.cookies.get("token")?.value ?? '';
+        if (token) {
+            let decoded = verifyToken(token);
+            if (!decoded) { // invalid token
+                return NextResponse.redirect(new URL('/', req.url));
+            }
+            if (decoded.user_type < UserType.ADMIN) { // not admin
+                return return_400('Permission denied');
+            }
+        }
+        else { // not logged in
+                return NextResponse.redirect(new URL('/', req.url));
+        }
+
+        const data = await req.json();
+        if (!data.user_id) {
+            return return_400('user_id is required');
+        }
+
+        let [user] =
+            await db.select()
+                .from(schema.usersTable)
+                .where(
+                    eq(schema.usersTable.uid, data.user_id)
+                );
+        if(!user) {
+            return return_400('User not found');
+        }
+        if (user.user_type != 0) {
+            return return_400('User has already been approved');
+        }
+
+        await db.update(schema.usersTable)
+            .set({user_type: 1})
+            .where(eq(schema.usersTable.uid, data.user_id));
+
+        return NextResponse.json({
+            success: true,
+            message: 'User approved'
+        });
+    } catch (e) {
+        console.error(e);
+        return NextResponse.json({
+            success: false,
+            message: 'Internal server error'
+        }, {status: 500});
+    }
+}
