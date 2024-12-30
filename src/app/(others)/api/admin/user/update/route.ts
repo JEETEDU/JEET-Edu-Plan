a31b50aecf8a6fa3b9dac30ba0b99d0aa5c6da1d
hegelty <skxodid0305@gmail.com>
2024-12-31 05:40:14 +0900

    Implement /admin/user/update and /admin/user/list

--- src/app/(others)/api/admin/user/update/route.ts
+++ src/app/(others)/api/admin/user/update/route.ts
@@ -8,11 +8,11 @@ import {verifyToken} from "@/app/(others)/api/auth";
 
 /**
  * @swagger
- * /api/admin/user/reject:
+ * /api/admin/user/update:
  *  post:
  *      tags:
  *          - Admin/User
- *      description: Reject a user
+ *      description: Update a user
  *      requestBody:
  *          required: true
  *          content:
@@ -24,11 +24,31 @@ import {verifyToken} from "@/app/(others)/api/auth";
  *                              type: number
  *                              description: User's ID
  *                              example: 1
+ *                          user_type:
+ *                              type: number
+ *                              description: User's type
+ *                              example: 1
+ *                          name:
+ *                              type: string
+ *                              description: User's name
+ *                              example: "John"
+ *                          first_year:
+ *                              type: number
+ *                              description: User's first year
+ *                              example: 2020
+ *                          school:
+ *                              type: string
+ *                              description: User's school
+ *                              example: "Gyeonggibuk Science High School"
+ *                          joined_term:
+ *                              type: string
+ *                              description: User's joined term
+ *                              example: "2020 Spring"
  *                      required:
  *                          - user_id
  *      responses:
  *          "200":
- *              description: User rejected
+ *              description: User updated
  *              content:
  *                  application/json:
  *                      schema:
@@ -39,7 +59,7 @@ import {verifyToken} from "@/app/(others)/api/auth";
  *                                  example: true
  *                              message:
  *                                  type: string
- *                                  example: "User rejected"
+ *                                  example: "User updated"
  *          "400":
  *              description: Bad request
  *              content:
@@ -74,27 +94,64 @@ export async function POST(req: NextRequest) {
             return return_400('user_id is required');
         }
 
+        const user_id = data.user_id;
+
         let [user] =
             await db.select()
                 .from(schema.usersTable)
                 .where(
-                    eq(schema.usersTable.uid, data.user_id)
+                    eq(schema.usersTable.uid, user_id)
                 );
+
         if (!user) {
             return return_400('User not found');
         }
-        if (user.user_type != 0) {
-            return return_400('User has already been approved');
+        if (user.user_type == 0) {
+            return return_400('User is not accepted');
+        }
+        if (user.user_type == 3) {
+            return return_400('Cannot update admin user');
         }
 
-        await db.delete(schema.usersTable)
-            .where(
-                eq(schema.usersTable.uid, data.user_id)
-            );
+        const user_type: number = data.user_type ?? 0;
+        const name: string = data.name ?? '';
+        const first_year: number = data.first_year ?? 0;
+        const school: string = data.school ?? '';
+        const joined_term: string = data.joined_term ?? '';
+
+        if (!user_type && !name && !first_year && !school && !joined_term) {
+            return return_400('No data to update');
+        }
+        if (user_type < 0 || user_type > 3) {
+            return return_400('Invalid user_type');
+        }
+        if (name && name.length > 5) {
+            return return_400('Name is too long');
+        }
+        if (first_year && (first_year < 1901 || first_year > 2100)) {
+            return return_400('Invalid first_year');
+        }
+        if (school && school.length > 255) {
+            return return_400('School is too long');
+        }
+        if (joined_term && joined_term.length > 20) {
+            return return_400('Joined_term is too long');
+        }
+
+        let update_data: any = {};
+        if (user_type) update_data['user_type'] = user_type;
+        if (name) update_data['name'] = name;
+        if (first_year) update_data['first_year'] = first_year;
+        if (school) update_data['school'] = school;
+        if (joined_term) update_data['joined_term'] = joined_term;
+
+        await db.update(schema.usersTable)
+            .set(update_data)
+            .where(eq(schema.usersTable.uid, user_id));
 
         return NextResponse.json({
             success: true,
-            message: "User rejected"
+            message: "User updated"
         });
     } catch (e) {
         console.error(e);
