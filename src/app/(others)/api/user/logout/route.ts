92fbeb70d27daf7bb3c3122b758992844bfc1f59
hegelty <skxodid0305@gmail.com>
2024-12-31 04:29:41 +0900

    Implement Logout and edit login/register

---
+++ src/app/(others)/api/user/logout/route.ts
@@ -0,0 +1,66 @@
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+import {return_400} from "@/app/(others)/api/tools";
+
+export async function GET(req: NextRequest) {
+    return POST(req);
+}
+
+/**
+ * @swagger
+ * /api/user/logout:
+ *  post:
+ *      tags:
+ *          - User
+ *      description: Logout
+ *      responses:
+ *          "200":
+ *              description: Logout successful
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              success:
+ *                                  type: boolean
+ *                                  example: true
+ *                              message:
+ *                                  type: string
+ *                                  example: "Logout successful"
+ *              headers:
+ *                  Set-Cookie:
+ *                      schema:
+ *                          type: string
+ *          "400":
+ *              description: Not logged in
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              success:
+ *                                  type: boolean
+ *                                  example: false
+ *                              message:
+ *                                  type: string
+ *                                  example: "Not logged in"
+ */
+export async function POST(req: NextRequest) {
+    try {
+        const token: string = req.cookies.get("token")?.value ?? '';
+        if (token) {
+            return NextResponse.json({
+                success: true,
+                message: "Logout successful"
+            }, {status: 200, headers: {"Set-Cookie": "token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT;"}});
+        } else {
+            return return_400("Not logged in");
+        }
+    } catch (e: any) {
+        console.error(e);
+        return NextResponse.json({
+            success: false,
+            message: "internal server error"
+        }, {status: 500});
+    }
+}
