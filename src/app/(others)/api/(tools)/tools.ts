1c794b46212b2d6f451b4a262ecfcd147ffd2ea8
hegelty <skxodid0305@gmail.com>
2025-01-01 02:39:10 +0900

    Make API responses further differentiated

--- src/app/(others)/api/(tools)/tools.ts
+++ src/app/(others)/api/(tools)/tools.ts
@@ -8,6 +8,20 @@ export function return_400(message: string): NextResponse {
             }, {status: 400});
 }
 
+export function return_not_logged_in(): NextResponse {
+    return NextResponse.json({
+                success: false,
+                message: "Not logged in"
+            }, {status: 401});
+}
+
+export function return_permission_denied(): NextResponse {
+    return NextResponse.json({
+                success: false,
+                message: "Permission denied"
+            }, {status: 403});
+}
+
 export enum UserType {
     NONE = 0,
     USER = 1,
