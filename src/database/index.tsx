2087ec83da3eaa9343f5deddce07c7959cb4fd02
hegelty <skxodid0305@gmail.com>
2025-01-17 14:19:29 +0900

    fix: too many connection(change db connection to singleton)

--- src/database/index.tsx
+++ src/database/index.tsx
@@ -2,12 +2,28 @@ import * as schema from "@/database/schema";
 import mysql from "mysql2/promise";
 import {drizzle} from "drizzle-orm/mysql2";
 
-export const poolConnection = mysql.createPool({
+// Singleton function to ensure only one db instance is created
+function singleton<Value>(name: string, value: () => Value): Value {
+    const globalAny: any = global;
+    globalAny.__singletons = globalAny.__singletons || {};
+
+    if (!globalAny.__singletons[name]) {
+      globalAny.__singletons[name] = value();
+    }
+
+    return globalAny.__singletons[name];
+}
+
+// Function to create the database connection and apply migrations if needed
+function createDatabaseConnection() {
+    const poolConnection = mysql.createPool({
     host: process.env["DB_HOST"],
     port: process.env["DB_PORT"],
     user: process.env["DB_USER"],
     password: process.env["DB_PSWD"],
     database: process.env["DB_NAME"],
 });
+    return drizzle(poolConnection);
+}
 
-export const db = drizzle(poolConnection, {schema, mode: 'planetscale'});
+export const db = singleton('db', createDatabaseConnection);
