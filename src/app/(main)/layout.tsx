2b152fe34ac09027a1452578224f6369f81dc9ad
ho-ya-kim <seoho7777.kim@gmail.com>
2025-01-21 16:49:50 +0900

    block scroll for body

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -5,6 +5,7 @@ import {cookies} from "next/headers";
 import Navigation from "@/app/(main)/components/desktop";
 import {Navigation1, Navigation2} from "@/app/(main)/components/mobile";
 import {CookiesProvider} from "next-client-cookies/server";
+import Body from "@/app/(main)/_layout";
 
 export const metadata: Metadata = {
     title: "JEET",
@@ -18,25 +19,25 @@ export default async function RootLayout(
     const isMobile = cookieStore.get("isMobile") ?? {value: null};
 
     return (
-
         <html lang="en">
-        <body className="flex flex-col h-screen min-h-screen max-h-screen">
-        <div className="static">
-            {isMobile?.value === 'true' ? <Navigation1/> : <Navigation/>}
-        </div>
-        <main className="flex-grow-1 overflow-hidden">
-            <Suspense fallback={
-                <div className="text-3xl font-bold w-full h-full flex justify-center items-center">
-                    로딩중...
-                </div>
-            }>
-                <CookiesProvider>{children}</CookiesProvider>
-            </Suspense>
-        </main>
-        <div className="static">
-            {isMobile?.value === 'true' ? <Navigation2/> : null}
-        </div>
-        </body>
+        <Body>
+            <div>
+                {isMobile?.value === 'true' ? <Navigation1/> : <Navigation/>}
+            </div>
+            <main className="flex-grow-1 overflow-hidden">
+                <Suspense fallback={
+                    <div className="text-3xl font-bold w-full h-full flex justify-center items-center">
+                        로딩중...
+                    </div>
+                }>
+                    <CookiesProvider>{children}</CookiesProvider>
+                </Suspense>
+            </main>
+            <div>
+                {isMobile?.value === 'true' ? <Navigation2/> : null}
+            </div>
+        </Body>
         </html>
     );
 }
+
