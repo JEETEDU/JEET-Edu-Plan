987494ce7626de65dc8f38f4a6bdf677e0b895bb
ho-ya-kim <seoho7777.kim@gmail.com>
2025-02-14 01:20:03 +0900

    made contact page

--- src/app/(main)/(links)/contact/page.tsx
+++ src/app/(main)/(links)/contact/page.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Scrollbars from "react-custom-scrollbars-2";
 import TextareaAutosize from "react-textarea-autosize";
 import {cn} from "@/app/(main)/components/functions";
 import {Nanum_Pen_Script, Ubuntu, Noto_Serif_KR} from "next/font/google";
 import Image from "next/image";
+import {sendContactEmail} from "@/app/(others)/api/email/(tools)/contact";
 
 const nanumPenScript = Nanum_Pen_Script({weight: "400", preload: false});
 const ubuntu = Ubuntu({weight: "400", preload: false});
@@ -13,6 +14,32 @@ const notoSerifKr = Noto_Serif_KR({weight: "700", preload: false});
 
 export default function Inquiry() {
     const [focus, setFocus] = useState<number>(0);
+    const [title, setTitle] = useState<string>("");
+    const [name, setName] = useState<string>("");
+    const [contact, setContact] = useState<string>("");
+    const [content, setContent] = useState<string>("");
+    const [error, setError] = useState<[boolean, boolean, boolean, boolean]>([false, false, false, false]);
+
+    useEffect(() => {
+        setError([false, false, false, false]);
+    }, [title, name, contact, content]);
+
+    function submit() {
+        if (!title.trim() || !name.trim() || !contact.trim() || !content.trim()) {
+            setError([!title.trim(), !name.trim(), !contact.trim(), !content.trim()]);
+            return;
+        }
+        sendContactEmail({
+            title: title,
+            name: name,
+            contact: contact,
+            content: content,
+        }).then(r => {
+            if (r.success) {
+                alert("문의가 접수되었습니다.");
+            }
+        });
+    }
 
     return (
         <div className="grid grid-rows-3 gap-4 px-20 py-4 h-full text-gray-600">
@@ -21,16 +48,35 @@ export default function Inquiry() {
                     문의하기
                 </div>
                 <div className="col-span-3 bg-white shadow-xl p-8 flex flex-col gap-4 m-2 hover:m-0">
-                    <div className="grid grid-cols-2 gap-4">
+                    <div className="grid grid-cols-4 gap-4">
+                        <div className="flex flex-col w-full col-span-2">
+                            <div className="component-button-info">
+                                제목
+                            </div>
+                            <div className={cn("border-2 rounded", {"border-gray": (focus === 4)}, {"border-red-500": error[0]})}>
+                                <input
+                                    type="text"
+                                    className="outline-none bg-white py-2 px-3 w-full"
+                                    placeholder={error[0] ? "제목이 비어있습니다." : "제목을 입력해 주세요"}
+                                    required
+                                    value={title}
+                                    onChange={(e) => setTitle(e.target.value)}
+                                    onFocus={() => setFocus(4)}
+                                    onBlur={() => setFocus(0)}
+                                />
+                            </div>
+                        </div>
                         <div className="flex flex-col w-full">
                             <div className="component-button-info">
                                 이름
                             </div>
-                            <div className={cn("border-2 rounded", {"border-gray": (focus === 1)})}>
+                            <div className={cn("border-2 rounded", {"border-gray": (focus === 1)}, {"border-red-500": error[1]})}>
                                 <input
                                     type="text"
                                     className="outline-none bg-white py-2 px-3 w-full"
-                                    placeholder="이름을 입력해 주세요"
+                                    placeholder={error[1] ? "이름이 비어있습니다." : "이름을 입력해 주세요"}
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
                                     required
                                     onFocus={() => setFocus(1)}
                                     onBlur={() => setFocus(0)}
@@ -39,13 +85,15 @@ export default function Inquiry() {
                         </div>
                         <div className="flex flex-col w-full">
                             <div className="component-button-info">
-                                연락처
+                                연락처 (Email or Call)
                             </div>
-                            <div className={cn("border-2 rounded", {"border-gray": (focus === 2)})}>
+                            <div className={cn("border-2 rounded", {"border-gray": (focus === 2)}, {"border-red-500": error[2]})}>
                                 <input
                                     type="text"
                                     className="outline-none bg-white py-2 px-3 w-full"
-                                    placeholder="연락처를 입력해 주세요"
+                                    placeholder={error[2] ? "연락처가 비어있습니다." : "연락처를 입력해 주세요"}
+                                    value={contact}
+                                    onChange={(e) => setContact(e.target.value)}
                                     required
                                     onFocus={() => setFocus(2)}
                                     onBlur={() => setFocus(0)}
@@ -57,7 +105,7 @@ export default function Inquiry() {
                         <div className="component-button-info">
                             문의 내용
                         </div>
-                        <div className={cn("flex-1 border-2 rounded", {"border-gray": (focus === 3)})}>
+                        <div className={cn("flex-1 border-2 rounded", {"border-gray": (focus === 3)}, {"border-red-500": error[3]})}>
                             <Scrollbars
                                 className="h-full"
                                 universal
@@ -65,11 +113,22 @@ export default function Inquiry() {
                             >
                                 <TextareaAutosize
                                     className="break-all resize-none min-h-full w-full bg-white outline-none px-3 py-2"
-                                    placeholder="문의사항을 입력해주세요"
+                                    placeholder={error[3] ? "내용이 비어있습니다." : "내용을 입력해 주세요"}
+                                    value={content}
+                                    onChange={(e) => setContent(e.target.value)}
+                                    required
                                     onFocus={() => setFocus(3)}
                                     onBlur={() => setFocus(0)}
                                 />
                             </Scrollbars>
+                        </div>
+                    </div>
+                    <div className="w-full flex justify-center items-center">
+                        <div
+                            className="py-1 px-8 border-2 rounded hover:bg-white bg-gray-200 cursor-pointer"
+                            onClick={() => submit()}
+                        >
+                            문의하기
                         </div>
                     </div>
                 </div>
@@ -114,7 +173,7 @@ export default function Inquiry() {
                                         <path fill="currentColor" d="M20 18h-2V9.25L12 13L6 9.25V18H4V6h1.2l6.8 4.25L18.8 6H20m0-2H4c-1.11 0-2 .89-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2"/>
                                     </svg>
                                     <div className="flex flex-col flex-1 text-blue-400 hover:text-blue-600">
-                                        dev@hegelty.me
+                                        skxodid0305@gmail.com
                                     </div>
                                 </div>
                                 <div className="flex flex-row w-full items-center justify-center gap-2">
@@ -155,7 +214,7 @@ export default function Inquiry() {
                                         <path fill="currentColor" d="M20 18h-2V9.25L12 13L6 9.25V18H4V6h1.2l6.8 4.25L18.8 6H20m0-2H4c-1.11 0-2 .89-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2"/>
                                     </svg>
                                     <div className="flex flex-col flex-1 text-blue-400 hover:text-blue-600">
-                                        seohokim@kaist.ac.kr
+                                        seoho7777.kim@gmail.com
                                     </div>
                                 </div>
                                 <div className="flex flex-row w-full items-center justify-center gap-2">
