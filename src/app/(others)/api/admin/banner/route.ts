54197fdb07fc9e981e76b31e7aff8ea8005ce95b
hegelty <skxodid0305@gmail.com>
2025-02-24 11:57:47 +0900

    fix: banner

--- src/app/(others)/api/admin/banner/route.ts
+++ src/app/(others)/api/admin/banner/route.ts
@@ -1,16 +1,46 @@
 import {NextRequest, NextResponse} from "next/server";
-import {DecodedToken, verifyToken} from "@/app/(others)/api/(tools)/auth";
-import {return_500, return_not_logged_in} from "@/app/(others)/api/(tools)/tools";
+import {DecodedToken} from "@/app/(others)/api/(tools)/auth";
+import {return_500} from "@/app/(others)/api/(tools)/tools";
 import fs from "node:fs";
+import {check_admin_permission} from "@/app/(others)/api/admin/(tools)/tools";
 
+/**
+ * @swagger
+ * /api/admin/banner:
+ *   put:
+ *     summary: Upload a banner image
+ *     description: Upload a banner image to the server.
+ *     tags:
+ *       - Admin
+ *     security:
+ *       - cookieAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               file:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       200:
+ *         description: The banner image has been uploaded successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ */
 export async function PUT(req: NextRequest) {
     try {
-        const token = req.cookies.get("token")?.value ?? '';
-        const decoded: DecodedToken | false = verifyToken(token);
-        if (!decoded) return return_not_logged_in();
+        const token: DecodedToken | NextResponse = check_admin_permission(req.cookies.get("token")?.value ?? '');
+        if (token instanceof NextResponse) return token;
 
         const data = await req.formData();
-        // const article = data.get("article");
         // @ts-ignore
         const file = data.get("file") as File;
         const file_path = 'uploads/banner/banner.png';
